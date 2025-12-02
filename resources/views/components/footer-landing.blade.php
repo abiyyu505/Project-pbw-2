eea26cdc5482646dc36ae1aa6ec3ab5cb1f50872
Ihsan Faiz <ihsanfaiz200@gmail.com>
2025-12-02 22:18:09 +0700

    landing page for progress 1

---
+++ resources/views/components/footer-landing.blade.php
@@ -0,0 +1,47 @@
+<div class="px-40 py-20 w-full h-[500px] bg-pink-100">
+    <h1 class="text-pink-700 font-bold text-3xl">HOTEL LINGIAN</h1>
+    <div class="grid grid-cols-4 mt-10">
+        {{-- container 1 --}}
+        <div class="flex flex-col gap-3">
+            <h2 class="text-pink-700 font-bold">HOTEL LINGIAN</h2>
+            <p class="text-gray-500 font-semibold">At Telkom University</p>
+            <a class="flex justify-center bg-pink-700 px-2 py-1 w-40 hover:bg-pink-800 text-center h-10 items-center font-bold text-white rounded-md transition-all duration-300" href="">Book Now</a>
+        </div>
+
+        {{-- container 2 --}}
+        <div>
+            <h2 class="font-bold">Social Media</h2>
+            <p class="text-gray-500 font-semibold">lorem ipsum</p>
+            <p class="text-gray-500 font-semibold">lorem</p>
+            <p class="text-gray-500 font-semibold">lorem ipsum</p>
+            <p class="text-gray-500 font-semibold">lorem</p>
+            <p class="text-gray-500 font-semibold">lorem ipsum</p>
+            <p class="text-gray-500 font-semibold">lorem</p>
+        </div>
+
+        {{-- container 3 --}}
+        <div>
+            <h2 class="font-bold">Our Partner</h2>
+            <p class="text-gray-500 font-semibold">lorem ipsum</p>
+            <p class="text-gray-500 font-semibold">lorem</p>
+            <p class="text-gray-500 font-semibold">lorem ipsum</p>
+            <p class="text-gray-500 font-semibold">lorem</p>
+            <p class="text-gray-500 font-semibold">lorem ipsum</p>
+            <p class="text-gray-500 font-semibold">lorem</p>
+        </div>
+
+        {{-- container 4 --}}
+        <div>
+            <h2 class="font-bold">Our Investor</h2>
+            <p class="text-gray-500 font-semibold">lorem ipsum</p>
+            <p class="text-gray-500 font-semibold">lorem</p>
+            <p class="text-gray-500 font-semibold">lorem ipsum</p>
+            <p class="text-gray-500 font-semibold">lorem</p>
+            <p class="text-gray-500 font-semibold">lorem ipsum</p>
+            <p class="text-gray-500 font-semibold">lorem</p>
+        </div>
+    </div>
+</div>
+<div class="bg-white w-full h-16 flex justify-center items-center">
+    <p class="font-semibold">Design by <span class="text-pink-700 font-bold">Ihsan Faiz</span></p>
+</div>
