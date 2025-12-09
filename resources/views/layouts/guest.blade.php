191a50cf922b55000e38f4fe0d7f7bf904f8b3da
Ihsan Faiz <ihsanfaiz200@gmail.com>
2025-12-09 14:29:45 +0700

    feat: login page

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,16 +15,26 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased bg-[#ededed]">
-        <div class="w-full relative overflow-hidden px-20 py-10">
+        <div class="flex flex-col w-full relative overflow-hidden">
 
             {{-- banner background --}}
-            <div class=" w-full absolute -z-10 inset-0 h-96 overflow-hidden bg-cover">
+            <div class=" w-full absolute -z-10 inset-0 h-full overflow-hidden bg-cover">
                 <div class="bg-black/25 inset-0 top-0 left-0 absolute w-full h-full object-cover"></div>
-                <img class=" object-cover w-full h-full" src="{{ asset('assets/dark-blue-sky.webp') }}" alt="">
+                <img class=" object-cover w-full h-full" src="{{ asset('assets/login-background.jpg') }}" alt="">
             </div>
 
-            <div class="flex flex-col sm:justify-center items-center pt-6 sm:pt-0 ">
-                <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            <div class="flex sm:justify-center items-center pt-6 sm:pt-0 min-h-[100vh] shadow-md">
+
+                {{-- side get started --}}
+                <div class="relative rounded-tl-md rounded-bl-md flex w-96 h-[500px] overflow-hidden">
+                    <img class=" object-cover w-full h-full" src="{{ asset('assets/login-background.jpg') }}" alt="">
+                    <div class="absolute left-[50%] ">
+                        <p class=" text-xl font-bold text-white ">Get Started</p>
+                    </div>
+                </div>
+
+                {{-- login form --}}
+                <div class="w-96 h-[500px] px-6 py-4 bg-white overflow-hidden  rounded-tr-md rounded-br-md">
                     {{ $slot }}
                 </div>
             </div>
