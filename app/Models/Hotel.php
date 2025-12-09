6f1aa666729f77cc7585ea6e9f20315b33945b81
Ihsan Faiz <ihsanfaiz200@gmail.com>
2025-12-09 19:37:37 +0700

    feat: models for tables

---
+++ app/Models/Hotel.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Hotel extends Model
+{
+    /** @use HasFactory<\Database\Factories\HotelFactory> */
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function location(){
+        return $this->belongsTo(Location::class);
+    }
+
+    public function room(){
+        return $this->hasMany(Room::class);
+    }
+
+}
---
+++ app/Models/Payment.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    protected $guarded = [];
+
+    public function booking(){
+        return $this->hasOne(Booking::class);
+    }
+}
